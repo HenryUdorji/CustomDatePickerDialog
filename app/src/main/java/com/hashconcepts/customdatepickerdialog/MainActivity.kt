dcf0f34bf50e4baf67437d44eeee17be5e225d5a
ifechukwu <ifechukwu.udorji@accelerexholdings.com>
2022-07-20 22:29:44 +0100

    almost completed

--- app/src/main/java/com/hashconcepts/customdatepickerdialog/MainActivity.kt
+++ app/src/main/java/com/hashconcepts/customdatepickerdialog/MainActivity.kt
@@ -1,24 +1,30 @@
 package com.hashconcepts.customdatepickerdialog
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.toUpperCase
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import com.hashconcepts.customdatepickerdialog.ui.theme.CustomDatePickerDialogTheme
+import com.hashconcepts.customdatepickerdialog.ui.theme.spinnerGradient
+import java.util.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,12 +32,26 @@ class MainActivity : ComponentActivity() {
         setContent {
             CustomDatePickerDialogTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
+                Box(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
                 ) {
-                    CustomDatePickerDialog(label = "Date of Birth") {
 
+                    var showDialog by remember { mutableStateOf(false) }
+                    Button(
+                        shape = RoundedCornerShape(10.dp),
+                        modifier = Modifier
+                            .padding(horizontal = 20.dp)
+                            .align(Alignment.Center),
+                        onClick = {
+                            showDialog = true
+                        }) {
+                        Text(text = "Date Picker", style = MaterialTheme.typography.body1)
+                    }
+
+                    if (showDialog) {
+                        CustomDatePickerDialog(label = "Date Picker") {
+                            showDialog = false
+                        }
                     }
                 }
             }
@@ -83,7 +103,8 @@ fun DatePickerUI(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(horizontal = 10.dp),
-                onClick = { /*TODO*/ }) {
+                onClick = { onDismissRequest() }
+            ) {
                 Text(
                     text = "Done",
                     style = MaterialTheme.typography.button,
@@ -96,53 +117,96 @@ fun DatePickerUI(
 }
 
 @Composable
-fun DateSelectionSection() {
+fun DateSelectionSection(
+    onYearChosen
+) {
+    val chosenYear = remember { mutableStateOf(currentYear) }
+    val chosenMonth = remember { mutableStateOf(currentMonth) }
+    val chosenDay = remember { mutableStateOf(currentDay) }
+
+
     Row(
-        horizontalArrangement = Arrangement.SpaceEvenly,
+        horizontalArrangement = Arrangement.SpaceAround,
         modifier = Modifier
             .fillMaxWidth()
             .height(120.dp)
     ) {
-        LazyColumn(content = {
-            items(getDayList()) { item ->
-                DOBItem(item)
-            }
-        })
+        InfiniteItemsPicker(
+            items = days,
+            firstIndex = Int.MAX_VALUE / 2 + (currentDay - 2),
+            onItemSelected = { chosenDay.value = it.toInt() }
+        )
 
-        LazyColumn(content = {
-            items(fullMonths()) { item ->
-                DOBItem(item)
-            }
-        })
+        InfiniteItemsPicker(
+            items = monthsNames,
+            firstIndex = Int.MAX_VALUE / 2 - 4 + currentMonth,
+            onItemSelected = { chosenMonth.value = monthsNames.indexOf(it) }
+        )
 
-        LazyColumn(content = {
-            items(getYearList()) { item ->
-                DOBItem(item)
-            }
-        })
+        InfiniteItemsPicker(
+            items = years,
+            firstIndex = Int.MAX_VALUE / 2 + (currentYear - 1967),
+            onItemSelected = { chosenYear.value = it.toInt() }
+        )
     }
 }
 
 @Composable
-fun DOBItem(day: String) {
-    Column(
-        verticalArrangement = Arrangement.SpaceEvenly,
-        modifier = Modifier.height(40.dp)
-    ) {
-        Text(text = day, style = MaterialTheme.typography.body1, modifier = Modifier.padding(10.dp))
+fun InfiniteItemsPicker(
+    modifier: Modifier = Modifier,
+    items: List<String>,
+    firstIndex: Int,
+    onItemSelected: (String) -> Unit,
+) {
+
+    val listState = rememberLazyListState(firstIndex)
+    val currentValue = remember { mutableStateOf("") }
+
+    LaunchedEffect(key1 = !listState.isScrollInProgress) {
+        onItemSelected(currentValue.value)
+        listState.animateScrollToItem(index = listState.firstVisibleItemIndex)
+    }
+
+    Box(modifier = Modifier.height(106.dp)) {
+        LazyColumn(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            state = listState,
+            content = {
+                items(count = Int.MAX_VALUE, itemContent = {
+                    val index = it % items.size
+                    if (it == listState.firstVisibleItemIndex + 1) {
+                        currentValue.value = items[index]
+                    }
+
+                    Spacer(modifier = Modifier.height(6.dp))
+
+                    Text(
+                        text = items[index],
+                        modifier = Modifier.alpha(if (it == listState.firstVisibleItemIndex + 1) 1f else 0.3f),
+                        style = MaterialTheme.typography.body1,
+                        textAlign = TextAlign.Center
+                    )
+
+                    Spacer(modifier = Modifier.height(6.dp))
+                })
+            }
+        )
     }
 }
 
+val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+val currentDay = Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+val currentMonth = Calendar.getInstance().get(Calendar.MONTH)
 
-fun getYearList() = (1960..2100).map { it.toString() }
-fun getMonthList() = (1..12).map { it.toString() }
-fun getDayList() = (1..31).map { it.toString() }
-
-fun fullMonths() = listOf(
+val years = (1950..2050).map { it.toString() }
+val monthsNumber = (1..12).map { it.toString() }
+val days = (1..31).map { it.toString() }
+val monthsNames = listOf(
     "Jan",
     "Feb",
     "Mar",
     "Apr",
+    "May",
     "Jun",
     "Jul",
     "Aug",
@@ -152,12 +216,5 @@ fun fullMonths() = listOf(
     "Dec"
 )
 
-@Preview(showBackground = true)
-@Composable
-fun DatePickerPreview() {
-    CustomDatePickerDialogTheme {
-        DatePickerUI(label = "Date of Birth") {
 
-        }
-    }
-}
+
--- app/src/main/java/com/hashconcepts/customdatepickerdialog/ui/theme/Color.kt
+++ app/src/main/java/com/hashconcepts/customdatepickerdialog/ui/theme/Color.kt
@@ -1,8 +1,20 @@
 package com.hashconcepts.customdatepickerdialog.ui.theme
 
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val spinnerGradient = Brush.linearGradient(
+    0.0f to Color(0xFFFFFFFF),
+    0.3f to Color(0x00000000),
+    0.5f to Color(0x00000000),
+    0.7f to Color(0x00000000),
+    1.0f to Color(0xFFFFFFFF),
+    start = Offset(0.0f, -13.0f),
+    end = Offset(0.0f, 320.0f)
+)
